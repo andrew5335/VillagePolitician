e8c26db2c699150075d15091a6211cf4064f1927
andrew5335 <andrew2767@gmail.com>
2019-02-25 10:22:09 +0900

    의원 정보 API 서비스 코드 추가

--- app/src/main/java/politics/andrew/com/villagepolitician/interfacevo/Congressman.java
+++ app/src/main/java/politics/andrew/com/villagepolitician/interfacevo/Congressman.java
@@ -20,50 +20,59 @@ public class Congressman implements Serializable {
 
     private static final long serialVersionUID = 2472869833554749578L;
 
-    @SerializedName("name_kr") String name_kr;    // 한글 이름
-    @SerializedName("name_cn") String name_cn;    // 한자 이름
-    @SerializedName("name_en") String name_en;    // 영문 이름
-    @SerializedName("birth") String birth;    // 생년월일
-    @SerializedName("party") String party;    // 소속정당
-    @SerializedName("district") String district;    // 지역구
-    @SerializedName("committee") String committee;    // 소속위원회
-    @SerializedName("when_elected") String when_elected;    // 당 회수
-    @SerializedName("off_phone") String off_phone;    // 전화번호
-    @SerializedName("homepage") String homepage;    // 홈페이지주소
-    @SerializedName("email") String email;    // 메일주소
-    @SerializedName("aides") String aides;    // 보좌관정보
-    @SerializedName("pr_secrs") String pr_secrs;    // 비서관
-    @SerializedName("sc_secrs") String sc_secrs;    // 비서
-    @SerializedName("hobby") String hobby;    // 취미
-    @SerializedName("experience") String experience;    // 경력사항
-    @SerializedName("photo") String photo;    // 사진 주소
-    @SerializedName("url") String url;    // 국회 홈페이지 상세 주소
+        @SerializedName("name_kr") String name_kr;    // 한글 이름
+        @SerializedName("name_cn") String name_cn;    // 한자 이름
+        @SerializedName("name_en") String name_en;    // 영문 이름
+        @SerializedName("birth") String birth;    // 생년월일
+        @SerializedName("party") String party;    // 소속정당
+        @SerializedName("district") String district;    // 지역구
+        @SerializedName("committee") String committee;    // 소속위원회
+        @SerializedName("when_elected") String when_elected;    // 당 회수
+        @SerializedName("off_phone") String off_phone;    // 전화번호
+        @SerializedName("homepage") String homepage;    // 홈페이지주소
+        @SerializedName("email") String email;    // 메일주소
+        @SerializedName("aides") String aides;    // 보좌관정보
+        @SerializedName("pr_secrs") String pr_secrs;    // 비서관
+        @SerializedName("sc_secrs") String sc_secrs;    // 비서
+        @SerializedName("hobby") String hobby;    // 취미
+        @SerializedName("experience") String experience;    // 경력사항
+        @SerializedName("photo") String photo;    // 사진 주소
+        @SerializedName("url") String url;    // 국회 홈페이지 상세 주소
 
-    public String getName_kr() { return name_kr; }
-    public String getName_cn() { return name_cn; }
-    public String getName_en() { return name_en; }
-    public String getBirth() { return birth; }
-    public String getParty() { return party; }
-    public String getDistrict() { return district; }
-    public String getCommittee() { return committee; }
-    public String getWhen_elected() { return when_elected; }
-    public String getOff_phone() { return off_phone; }
-    public String getHomepage() { return homepage; }
-    public String getEmail() { return email; }
-    public String getAides() { return aides; }
-    public String getPr_secrs() { return pr_secrs; }
-    public String getSc_secrs() { return sc_secrs; }
-    public String getHobby() { return hobby; }
-    public String getExperience() { return experience; }
-    public String getPhoto() { return photo; }
-    public String getUrl() { return url; }
+        public String getName_kr() { return name_kr; }
+        public String getName_cn() { return name_cn; }
+        public String getName_en() { return name_en; }
+        public String getBirth() { return birth; }
+        public String getParty() { return party; }
+        public String getDistrict() { return district; }
+        public String getCommittee() { return committee; }
+        public String getWhen_elected() { return when_elected; }
+        public String getOff_phone() { return off_phone; }
+        public String getHomepage() { return homepage; }
+        public String getEmail() { return email; }
+        public String getAides() { return aides; }
+        public String getPr_secrs() { return pr_secrs; }
+        public String getSc_secrs() { return sc_secrs; }
+        public String getHobby() { return hobby; }
+        public String getExperience() { return experience; }
+        public String getPhoto() { return photo; }
+        public String getUrl() { return url; }
 
-    public interface CongressmanInterface {
+    public interface CongressmanListInterface {
         @GET("/politician/congressman/v1/1.0.0/congressmanList.php")
         Call<List<Congressman>> get_congressman_list(
           @Query("page") int page
           , @Query("per_page") int per_page
           , @Query("apiKey") String apiKey
+        );
+    }
+
+    public interface CongressmanInterface {
+        @GET("/politician/congressman/v1/1.0.0/contressmanList.php")
+        Call<Congressman> get_congressman(
+                @Query("page") int page
+                , @Query("per_page") int per_page
+                , @Query("apiKey") String apiKey
         );
     }
 }
