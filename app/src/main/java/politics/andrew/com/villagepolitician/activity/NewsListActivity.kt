2af186ddc639b51c12dc354588efcd7a52532ac0
andrew5335 <andrew2767@gmail.com>
2019-03-25 15:30:32 +0900

    news list 용 activity 생성

---
+++ app/src/main/java/politics/andrew/com/villagepolitician/activity/NewsListActivity.kt
@@ -0,0 +1,17 @@
+package politics.andrew.com.villagepolitician.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import politics.andrew.com.villagepolitician.R
+
+class NewsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_list)
+
+        val newsIntent = intent
+
+
+    }
+}
